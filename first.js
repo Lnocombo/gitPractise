a098b41d2d1c8d8771616c289cb9389ad21fdaec
Lnocombo <erty.2008@qq.com>
2018-02-09 10:19:23 +0800

    fetch practising

---
+++ first.js
@@ -0,0 +1,16 @@
+/**
+ * branch_dev
+ * first file
+ */ 
+/**
+ * The branch was created by local.
+ * It`s for practising merge remote branch into another local branch fix at local repository.
+ */
+
+/** 
+ * (operations in local branch fix)
+ * firt step 
+ * git fetch origin
+ * secode step
+ * git merge dev 
+*/
